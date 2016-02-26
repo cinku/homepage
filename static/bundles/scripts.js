4858252dafff17dda99c9c30782ebd2f53f14695
cinkowski <cinq2ez@gmail.com>
2016-02-26 11:09:33 -0800

    Menu item now shows which url we are on

--- static/bundles/scripts.js
+++ static/bundles/scripts.js
@@ -422,13 +422,40 @@ homepageApp.config(['$routeProvider', '$locationProvider', function($routeProvid
 'use strict';
 
 angular.module('directives', [])
-.directive("menuDirective", function() {
-  function link(scope, el, attrs) {
-    el.on('click', function() {
-      console.log('test');
+.directive('navMenu', function($location) {
+  return function(scope, element, attrs) {
+    var links = element.find('a'),
+        onClass = 'active',
+        routePattern,
+        link,
+        url,
+        currentLink,
+        urlMap = {},
+        i;
+
+    if (!$location.$$html5) {
+      routePattern = /^#[^/]*/;
+    }
+
+    for (i = 0; i < links.length; i++) {
+      link = angular.element(links[i]);
+      url = link.attr('href');
+      if ($location.$$html5) {
+        urlMap[url] = link;
+      } else {
+        urlMap[url.replace(routePattern, '')] = link;
+      }
+    }
+
+    scope.$on('$routeChangeStart', function() {
+      var pathLink = urlMap[$location.path()];
+      if (pathLink) {
+        if (currentLink) {
+          currentLink.removeClass(onClass);
+        }
+        currentLink = pathLink;
+        currentLink.addClass(onClass);
+      }
     });
-  }
-  return {
-    link: link
-  }
+  };
 });
