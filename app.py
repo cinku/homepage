839d3a5ced0931eef4e5e41de645af42e673b5aa
cinkowski <cinq2ez@gmail.com>
2016-03-04 19:27:43 +0100

    Modify ng-view to make use of animate.css animations

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ scripts = Bundle('scripts/vendor/angular.min.js', 'scripts/vendor/angular-route.
                  'scripts/vendor/jquery.min.js', 'scripts/vendor/bootstrap.min.js',
                  'scripts/app/app.js', 'scripts/app/directives/directives.js',
                  output='bundles/scripts.js')
-styles = Bundle('css/vendor/bootstrap.min.css', 'css/site.css', 'css/vendor/animate.css',
+styles = Bundle('css/vendor/bootstrap.min.css', 'css/site.css',
                 output='bundles/styles.css')
 assets.register('_scripts', scripts)
 assets.register('_styles', styles)
